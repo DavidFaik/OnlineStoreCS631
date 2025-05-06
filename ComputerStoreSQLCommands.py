482fac2a695418ee808cca675a0c5967df3944a2
kimmieharding <kah46@njit.edu>
2025-05-06 13:10:00 -0400

    CREATE TABLES

---
+++ ComputerStoreSQLCommands.py
@@ -0,0 +1,88 @@
+##Constaints for SQL Table Definitions
+##Note: must populate tables first with basic data than add foregin keys to avoid referencing problems
+
+class ComputerStoreSQlConstants:
+    """
+    A class containing SQL constants for the Computer Store application.
+    """
+    SILVER_AND_ABOVE_DEF = """CREATE TABLE IF NOT EXIST SILVER_AND_ABOVE
+                    (CID CHAR(10) NOT NULL,
+                    CREDITLINE INT ,
+                    FOREIGN KEY (CID) REFERENCES CUSTOMERS(CID)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE)"""
+
+    CUSTOMER_DEF = """CREATE TABLE IF NOT EXIST CUSTOMER
+            (CID CHAR(10) NOT NULL,
+            FNAME VARCHAR(15) NOT NULL,
+            LNAME VARCHAR(15) NOT NULL,
+            EMAIL VARCHAR(30),
+            ADDRESS VARCHAR(30),
+            PHONE CHAR(10),
+            STATUS CHAR NOT NULL DEFAULT'R')"""
+
+    CREDIT_CARD_DEF = """CREATE TABLE IF NOT EXIST CREDIT_CARD
+                (CCNUMBER CHAR(16) NOT NULL,
+                SECNUMBER CHAR(3) NOT NULL,
+                OWNERNAME VARCHAR(15) NOT NULL,
+                CCType VARCHAR(10) NOT NULL,
+                BILLADDRESS VARCHAR(30),
+                EXPDATE DATE,
+                STOREDCARDCID CHAR(10)
+                )"""
+
+    SHIPPING_ADDRESS_DEF = """CREATE TABLE IF NOT EXIST SHIPPING_ADDRESS
+                    (CID CHAR(10) NOT NULL,
+                    SANAME VARCHAR(30) NOT NULL,
+                    RECEPIENTNAME VARCHAR(30),
+                    STREET VARCHAR(10),
+                    SNUMBER INT,
+                    CITY VARCHAR(15),
+                    ZIP CHAR(5),
+                    STATE CHAR(2),
+                    COUNTRY VARCHAR(15))"""
+
+    TRANSACTIONS_DEF = """CREATE TABLE IF NOT EXIST TRANSACTIONS
+                (BID CHAR(10) NOT NULL,
+                CCNUMBER CHAR(16) NOT NULL,
+                CID CHAR(10) NOT NULL,
+                SANAME VARCHAR(30) NOT NULL,
+                TDATE DATE,
+                TTAG CHAR DEFAULT'C')""" ##TTAG options assume C=confirmed, S=Shipped, E=Enroute, D=Delivered, L=Lost
+
+    BASKET_DEF = """CREATE TABLE IF NOT EXIST BASKET
+            (CID CHAR(10) NOT NULL,
+            BID CHAR(10) NOT NULL)"""
+
+    APPEARS_IN_DEF = """CREATE TABLE IF NOT EXIST APPEARS_IN
+                (BID CHAR(10) NOT NULL,
+                PID CHAR(10) NOT NULL,
+                QUANTITY INT,
+                PRICESOLD DECIMAL(10,2))"""
+
+    OFFER_PRODUCT_DEF = """CREATE TABLE IF NOT EXIST OFFER_PRODUCT
+                    (PID CHAR(10) NOT NULL,
+                    OFFERPRICE DECIMAL(10,2))"""
+
+    PRODUCT_DEF = """CREATE TABLE IF NOT EXIST PRODUCT
+                (PID CHAR(10) NOT NULL,
+                PType CHAR, 
+                PNAME VARCHAR(15),
+                PPRICE DECIMAL(10,2),
+                DESCRIPTION VARCHAR(15),
+                PQUANTITY INT)""" ##PType options assume C=Computer P=Printer L=Laptop M=Miscelenous
+
+    COMPUTER_DEF = """CREATE TABLE IF NOT EXIST COMPUTER
+                CREATE TABLE COMPUTER
+                (PID CHAR(10) NOT NULL,
+                CPUTYPE VARCHAR(10))"""
+
+    PRINTER_DEF = """CREATE TABLE IF NOT EXIST PRINTER
+                (PID CHAR(10) NOT NULL,
+                PRINTER TYPE VARCHAR(10),
+                RESOLUTION VARCHAR(10))"""
+
+    LAPTOP_DEF = """CREATE TABLE IF NOT EXIST LAPTOP
+                (PID CHAR(10) NOT NULL,
+                BTYPE VARCHAR(15),
+                WEIGHT INT)"""
---
+++ DatabaseSchema.py
@@ -0,0 +1,56 @@
+import mysql.connector
+from ComputerStoreSQLCommands import ComputerStoreSQlConstants
+
+class DatabaseSchema:
+    def __init__(self, host, user, password):
+        self.sql = ComputerStoreSQlConstants()
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+        self.DB_NAME = "OnlineComputerStore"
+        self.cursor = self.conn.cursor()
+        self.create_tables()
+        self.populate_tables()
+        self.update_constraints()
+
+    def create_database(self):
+        self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.DB_NAME}")
+        self.conn.database = self.DB_NAME
+
+    def create_tables(self):
+        tables = {
+            "CUSTOMERS": self.sql.CUSTOMER_DEF,
+            "SILVER_AND_ABOVE": self.sql.SILVER_AND_ABOVE_DEF,
+            "CREDIT_CARD": self.sql.CREDIT_CARD_DEF,
+            "SHIPPING_ADDRESS": self.sql.SHIPPING_ADDRESS_DEF,
+            "TRANSACTIONS": self.sql.TRANSACTIONS_DEF,
+            "BASKET": self.sql.BASKET_DEF,
+            "APPEARS_IN": self.sql.APPEARS_IN_DEF,
+            "OFFER_PRODUCT": self.sql.OFFER_PRODUCT_DEF,
+            "PRODUCT": self.sql.PRODUCT_DEF,
+            "COMPUTER": self.sql.COMPUTER_DEF,
+            "PRINTER": self.sql.PRINTER_DEF,
+            "LAPTOP": self.sql.LAPTOP_DEF
+        }
+
+        for table_name, ddl in tables.items():
+            print(f"Creating table {table_name}...")
+            self.cursor.execute(ddl)
+
+    def populate_tables(self):
+        pass
+
+    def update_constraints(self):
+        pass
+
+if __name__ == "__main__":
+    schema = DatabaseSchema(
+        host="localhost",
+        user="root",
+        password="KHlovesburton13!",
+        db_name="OnlineStoreDB"
+    )
+    schema.close()
+
